e58a9333bddd7af2a476ce9212e73fd1fd96913b
Bartłomiej Boczyło <120282905+Bartlomiej28@users.noreply.github.com>
2024-09-09 10:58:27 +0200

    Update Comment.tsx

--- frontend/src/Components/Comment.tsx
+++ frontend/src/Components/Comment.tsx
@@ -8,12 +8,12 @@ interface CommentType{
 
 function Comment({name, text, image} : CommentType) {
   return (
-    <div className="aspect-video p-8 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
+    <div className="aspect-video p-8 border border-gray-100 rounded-3xl bg-white shadow-2xl shadow-gray-600/10">
     <div className="flex gap-4 items-center">
       <div className="w-12 h-12">
         <img className="w-12 h-12 object-cover rounded-full" alt='comment' src={image} loading="lazy"/>
       </div>
-        <h6 className="text-lg font-medium text-gray-700 dark:text-white">{name}</h6>
+        <h6 className="text-lg font-medium text-gray-700">{name}</h6>
     </div>
     <p className="mt-8">{text}</p>
   </div>
